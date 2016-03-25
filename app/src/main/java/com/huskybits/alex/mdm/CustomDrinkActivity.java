88e42bcb3ab6be1a158f87cce348553e1de737d3
Andrei Carlutz <papuc.apc@gmail.com>
2016-03-25 09:52:51 +0200

    ANDREI STYLE

--- app/src/main/java/com/huskybits/alex/mdm/CustomDrinkActivity.java
+++ app/src/main/java/com/huskybits/alex/mdm/CustomDrinkActivity.java
@@ -26,6 +26,7 @@ public class CustomDrinkActivity extends Activity{
     private TextView textProgressThirdDrink;
     private TextView textProgressTemp;
 
+    // LEL
     // Button
     Button btnNext;
 
